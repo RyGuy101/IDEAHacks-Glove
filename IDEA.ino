ed11839db3c6e6ad67efe6430afc970b166abe39
Zac Cadarette <33590318+AsFastAsLight@users.noreply.github.com>
2018-01-13 15:19:30 -0800

    Update IDEA.ino

--- IDEA.ino
+++ IDEA.ino
@@ -15,11 +15,45 @@ void setup()
   for (int i = 0; i < numFlexes; i++) {
     pinMode(FLEX_PIN[i], INPUT);
   }
+  float tempAngle;
+  int x = 0;
 }
 
 void loop() 
 {
-  for (int i = 0; i < numFlexes; i++) {
+  int flexADC = analogRead(FLEX_PIN[1)];
+  float flexV = flexADC * VCC / 1023.0;
+  float flexR = ((R_DIV * VCC) / flexV) - R_DIV;
+  float angle = map(flexR, STRAIGHT_RESISTANCE[1], BEND_RESISTANCE[1],
+                     0, 90.0);
+  if(angle < 45) /*flex resistor is in a certain threshold */
+  {
+    delay(250);
+  }
+  else
+  {
+    int flexADC = analogRead(FLEX_PIN[0];
+    float flexV = flexADC * VCC / 1023.0;
+    float flexR = ((R_DIV * VCC) / flexV) - R_DIV;
+    float angle = map(flexR, STRAIGHT_RESISTANCE[0], BEND_RESISTANCE[0],
+                     0, 90.0);
+    if(x < 1)
+    {
+      tempAngle = angle;
+      x++;
+    }
+    if(angle > tempAngle)
+    {
+      // dimmer
+    }
+    else
+    {
+      // brighter
+    }
+    delay(250);
+  }
+
+  /* for (int i = 0; i < numFlexes; i++) {
     int flexADC = analogRead(FLEX_PIN[i]);
     float flexV = flexADC * VCC / 1023.0;
     float flexR = ((R_DIV * VCC) / flexV) - R_DIV;
@@ -32,10 +66,9 @@ void loop()
     Serial1.println("Bend " + String(i+1) + ": " + String(angle) + " degrees");
 
     Serial.println();
-  }
+  }*/
 
   Serial.println();
   Serial1.println();
   delay(500);
 }
-
